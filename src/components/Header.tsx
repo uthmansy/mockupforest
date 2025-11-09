b0f752ec8aa1d20f574103c94733cd2bd833d66e
shuaibu usman <uthmansy@gmail.com>
2025-11-09 12:44:43 +0100

    updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -81,33 +81,8 @@ export default function Header() {
             </button>
           </div>
           {/* Mobile Search */}
-          <div className="px-6 mb-6">
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="Search mockups..."
-                className="pl-10 pr-4 py-2 w-full border border-gray-700 bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-primary"
-                onClick={() => {
-                  /* keep open */
-                }}
-              />
-              <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </span>
-            </div>
+          <div className="px-6 mb-6 mx-auto">
+            <SearchBox />
           </div>
           <nav className="flex flex-col items-center justify-center flex-1 space-y-6">
             {NAV_ITEMS.map((item) => (
